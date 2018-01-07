0193d65ff77902b3e8c6e663a38b62fdd3ba5198
Myers-Omri <omri.myers@gmail.com>
2018-01-07 21:52:03 +0200

    add address updated transaction

--- src/main/java/DataTypes/Transaction.java
+++ src/main/java/DataTypes/Transaction.java
@@ -1,8 +1,45 @@
 package DataTypes;
 
-import static java.lang.Math.random;
 
 public class Transaction {
-    double data = random();
+    public enum TransactionType {
+        INIT_SHIPMENT,
+        LOCATION_UPDATE,
+        ARRIVED,
+        COLLECTED_BY_CUSTOMER
+    }
+
+    public TransactionType _type;
+    public int _transactionId;
+    public int _clientId; //external client- the one that uses ds-shipping service
+    public int _itemId;
+    public int _officeId; //internal client- the one that update the BlockChain for parcel location.
+
+
+     // type dependent fields:
+    // for INIT_SHIPMENT:
+    public Address _from;
+    public Address _to;
+    public String _description;
+    //public String _shipApproval;
+
+    // for LOCATION_UPDATE
+    public Address _currentLocation;
+    public Address _nextHop;
+
+    //for COLLECTED_BY_CUSTOMER:
+    //public String _arrivedApproval;
+
+    Transaction(int transaction_id, int item_id, int client_id, TransactionType type ){
+        _transactionId = transaction_id;
+        _itemId = item_id;
+        _clientId = client_id;
+        _type = type;
+    }
+
+
+
+    //double data = random();
+
 
 }
