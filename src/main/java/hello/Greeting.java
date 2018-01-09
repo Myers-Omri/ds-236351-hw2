78e4723f23d681af26ad17c2432dc7cc26ff3b3e
Unknown <alons111@gmail.com>
2018-01-09 19:51:04 +0200

    Spring integration

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,7 +1,20 @@
 package hello;
 
-/**
- * Created by salons on 09-Jan-18.
- */
 public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
