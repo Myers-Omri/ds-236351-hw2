f325d59ac64637836c37f4c6de35ede6401f41be
Unknown <alons111@gmail.com>
2018-01-09 22:56:08 +0200

    Added BlockChainserver to Spring app

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,26 @@
 package hello;
 
 import BlockChain.BlockChainServer;
+import DataTypes.Block;
+import Utils.Config;
+import Utils.SystemUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class Application {
     public static BlockChainServer server;
 
     public static void main(String[] args) {
+        Config.init();
+        SystemUtils.init();
+        try {
+            server = new BlockChainServer(Config.s_name, Config.addr, new Block(0), Config.id, Config.p_num);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(Application.class, args);
     }
 }
