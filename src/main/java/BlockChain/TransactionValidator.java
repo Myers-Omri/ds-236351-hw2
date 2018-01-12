1edc58b755d7553e6da67136360748ac01768e95
Myers-Omri <omri.myers@gmail.com>
2018-01-12 15:56:55 +0200

    adding basic transactionValidator need to add triger for paxox

---
+++ src/main/java/BlockChain/TransactionValidator.java
@@ -0,0 +1,74 @@
+package BlockChain;
+
+import DataTypes.Block;
+import DataTypes.Transaction;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class TransactionValidator {
+    private List<Block> blockchain = new ArrayList<>();
+    private Block currentBlock;
+    private Transaction currentTransaction;
+    private static Logger log = Logger.getLogger(Block.class.getName());
+    public TransactionValidator(List<Block> blockchainRef){
+        blockchain = blockchainRef;
+        currentBlock = null;
+        currentTransaction = null;
+    }
+
+    public void setCurrentBlock(Block currentBlock) {
+        this.currentBlock = currentBlock;
+    }
+
+//    public void setCurrentTransaction(Transaction currentTransaction){
+//        this.currentTransaction = currentTransaction;
+//    }
+
+    public boolean Validate(Transaction currentTransaction){
+        this.currentTransaction = currentTransaction;
+        for (Block b : blockchain){
+            for (Transaction t : b.transactions){
+                if (!(validateIds(t) && validateFromTo(t))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean validateFromTo(Transaction t) {
+        boolean res = true;
+        if (t.serviceId.equals(currentTransaction.serviceId)){
+            res = (t.getFrom().equals(currentTransaction.getFrom())) &&
+                    (t.getTo().equals(currentTransaction.getTo()));
+        }
+        if (! res){
+            log.info(format("(validateFromTo) Invalid transaction ", t.toString()));
+        }
+        return res;
+    }
+
+    private boolean validateIds(Transaction t) {
+        boolean res = true;
+
+        if (t.getTransactionId() == currentTransaction.getTransactionId()){
+            res =  (t.getClientId() == currentTransaction.getClientId()) &&
+                    (t.getItemId() == currentTransaction.getItemId());
+        }
+
+        if (t.serviceId.equals(currentTransaction.serviceId)){
+            res = (t.getClientId() == currentTransaction.getClientId()) &&
+                    (t.getItemId() == currentTransaction.getItemId() );
+        }
+        if (! res){
+            log.info(format("(validateIds) Invalid transaction ", t.toString()));
+        }
+        return res; //the transaction id is diffrent so no problem
+    }
+
+
+}
