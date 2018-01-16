9a8a98aa8ce4edff328f3c118622ca223fcfe785
Yehonatan Buchnik <yontyon@gmail.com>
2018-01-16 19:58:00 +0200

    final final commit

--- update.ps1
+++
@@ -1,6 +0,0 @@
-﻿$serversClusterFolder = "..\BCServersCluster"
-$targetFolder = "target"
-$servers = ls ..\BCServersCluster
-$servers | % { Copy-Item -Force -Recurse $targetFolder $serversClusterFolder\$_}
-$servers | % {Copy-Item -Force $serversClusterFolder\$_\"config.properties" $serversClusterFolder\$_\"target"\"classes"}
-$servers | % {Copy-Item -Force $serversClusterFolder\$_\"application.properties" $serversClusterFolder\$_\"target"\"classes"}
